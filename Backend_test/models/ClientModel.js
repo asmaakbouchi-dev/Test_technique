38872237fa84c969901ce8225505bbbdd9eb4adf
asmaakbouchi <asmaakbouchi@gmail.com>
2024-10-02 00:05:14 +0200

    Création de la base de données MongoDB nommée gestion_client

---
+++ Backend_test/models/ClientModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const clientSchema = new mongoose.Schema(
+  {
+    nom: {
+      type: String,
+      required: true,
+    },
+    prenom: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    motdepasse: {
+      type: String,
+      required: true,
+    },
+    telephone: {
+      type: String,
+      required:true
+    },
+    adresse: {
+      type: String,
+      required:true
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const clientModel = mongoose.model("Client",clientSchema);
+module.exports = clientModel
