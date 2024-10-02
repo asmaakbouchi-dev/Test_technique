a27454b658bf5cff53cf8295ec247f722a4ac614
asmaakbouchi <asmaakbouchi@gmail.com>
2024-10-02 15:24:36 +0200

    fair des changement de commentaite  au niveau de serveur.js

--- Backend_test/server.js
+++ Backend_test/server.js
@@ -4,7 +4,7 @@ const port=3000
 
 const clientRouter=require('./routes/ClientRoute') // importer routes
 const Erreur404 =require('./middlewares/errorMiddleware') // importer ficheer de gestion d'erreur
-require ('./config/database'); //importer l base de données
+require ('./config/database'); //importer la base de données
 
 app.use(express.json());
 app.use('/clients',clientRouter)
